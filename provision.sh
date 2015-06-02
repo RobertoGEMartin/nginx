3bf3425333b6b79263ab2197c4e50c51282acf7a
@rgem52 <rgomez@hi-iberia.es>
2015-06-02 18:49:45 +0200

    Vagrant: Setup nginx in the web-dev-env to load site from default vagrant synced folder in a www folder

--- provision.sh
+++ provision.sh
@@ -2,4 +2,10 @@ apt-get -y update
 
 apt-get -y install nginx
 
+#remove nginx root directory from nginx server
+rm -rf /usr/share/nginx/www
+#link VM root directory to old nginx root directory
+ln -s /vagrant/www /usr/share/nginx/www
+
 service nginx start
+
